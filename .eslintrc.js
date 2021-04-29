51297475d521dddfaeb08bd959bfed581370a104
Raul Stelescu <stelescuraul@gmail.com>
2021-04-29 15:50:50 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'prettier/prettier': ['error'],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'max-len': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE', 'StrictPascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      { selector: 'enumMember', format: ['camelCase', 'UPPER_CASE', 'PascalCase'] },
+      { selector: 'parameter', format: ['camelCase', 'UPPER_CASE', 'PascalCase'] },
+    ],
+  },
+  overrides: [
+    {
+      files: ['./src/database/migrations/*.ts', './src/database/seed/*.ts'],
+      rules: {
+        '@typescript-eslint/camelcase': 'off',
+      },
+    },
+  ],
+};
