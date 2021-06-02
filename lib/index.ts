d2c8c9bd6b32eda7736c11d48d20cd70f44d7632
Raul Stelescu <raul@avallone.io>
2021-06-02 14:45:25 +0200

    chore: export typeorm modules

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,3 @@
 export * from './rls.constants';
 export * from './rls.module';
+export * from './common';
--- test/nestjs/src/app.controller.ts
+++ test/nestjs/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
