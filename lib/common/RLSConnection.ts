876174dfebbf327b9c3206d1fddc942cc5d50bc4
Raul Stelescu <raul@avallone.io>
2021-09-10 15:39:15 +0200

    fix: copy inverse metadata

--- lib/common/RLSConnection.ts
+++ lib/common/RLSConnection.ts
@@ -1,5 +1,4 @@
 import { Connection, EntityMetadata } from 'typeorm';
-import { RelationMetadata } from 'typeorm/metadata/RelationMetadata';
 import { RLSPostgresDriver } from '../common/RLSPostgresDriver';
 import {
   ActorId,
@@ -26,18 +25,21 @@ export class RLSConnection extends Connection {
     this.tenantId = tenancyModelOptions.tenantId;
     this.actorId = tenancyModelOptions.actorId;
 
-    const metadatas = [];
+    const metadatas: EntityMetadata[] = [];
 
     this.entityMetadatas.forEach(em => {
-      const wrappedMetadata = Object.assign({}, EntityMetadata.prototype, em, {
-        connection: this,
-      });
+      // copy metadata and overwrite connection to this
+      const wrappedMetadata: EntityMetadata = Object.assign(
+        Object.create(Object.getPrototypeOf(em)),
+        em,
+        { connection: this },
+      );
 
+      // copy relations and overwrite the connection to this
       const metadataRelations = [];
       wrappedMetadata.relations.forEach(relation => {
         const wrappedRelation = Object.assign(
-          {},
-          RelationMetadata.prototype,
+          Object.create(Object.getPrototypeOf(relation)),
           relation,
         );
 
@@ -48,6 +50,34 @@ export class RLSConnection extends Connection {
       Object.assign(wrappedMetadata, { relations: metadataRelations });
       metadatas.push(wrappedMetadata);
     });
+
+    /**
+     * for each metadata we need to go through the relationsWithJoinColumns
+     * create a copy of it and find the inverseEntityMetadata that it
+     * references and assign our own copy to it. This will help the ===
+     * check on the topological sort, allowing for `save` and `delete`
+     * operations
+     */
+    for (const metadata of metadatas) {
+      const relationsWithJoinColumns = [];
+      metadata.relationsWithJoinColumns.forEach(rwjc => {
+        const relationWithJoinColumn = Object.assign(
+          Object.create(Object.getPrototypeOf(rwjc)),
+          rwjc,
+        );
+
+        const inverseEntityMetadata = metadatas.find(
+          m => m.name === relationWithJoinColumn.inverseEntityMetadata.name,
+        );
+
+        relationWithJoinColumn.inverseEntityMetadata = inverseEntityMetadata;
+        relationsWithJoinColumns.push(relationWithJoinColumn);
+      });
+
+      Object.assign(metadata, {
+        relationsWithJoinColumns,
+      });
+    }
 
     Object.assign(this, { entityMetadatas: metadatas });
 
