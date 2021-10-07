8a5668200d54f914dd0ff25f91e5a99d83bdfdd6
Raul Stelescu <raul@avallone.io>
2021-10-07 14:09:55 +0200

    fix: running rls set on transaction rollback

--- lib/common/RLSPostgresQueryRunner.ts
+++ lib/common/RLSPostgresQueryRunner.ts
@@ -1,6 +1,7 @@
 import { ReplicationMode } from 'typeorm';
 import { PostgresDriver } from 'typeorm/driver/postgres/PostgresDriver';
 import { PostgresQueryRunner } from 'typeorm/driver/postgres/PostgresQueryRunner';
+import { IsolationLevel } from 'typeorm/driver/types/IsolationLevel';
 import {
   ActorId,
   TenancyModelOptions,
@@ -10,6 +11,7 @@ import {
 export class RLSPostgresQueryRunner extends PostgresQueryRunner {
   tenantId: TenantId = null;
   actorId: ActorId = null;
+  isTransactionCommand = false;
 
   constructor(
     driver: PostgresDriver,
@@ -26,12 +28,35 @@ export class RLSPostgresQueryRunner extends PostgresQueryRunner {
   }
 
   async query(queryString: string, params?: any[]): Promise<any> {
-    await super.query(
-      `set "rls.tenant_id" = '${this.tenantId}'; set "rls.actor_id" = '${this.actorId}';`,
-    );
+    if (!this.isTransactionCommand) {
+      await super.query(
+        `set "rls.tenant_id" = '${this.tenantId}'; set "rls.actor_id" = '${this.actorId}';`,
+      );
+    }
 
     const result = await super.query(queryString, params);
-    await super.query(`reset rls.actor_id; reset rls.tenant_id;`);
+
+    if (!this.isTransactionCommand) {
+      await super.query(`reset rls.actor_id; reset rls.tenant_id;`);
+    }
     return result;
+  }
+
+  async startTransaction(isolationLevel?: IsolationLevel): Promise<void> {
+    this.isTransactionCommand = true;
+    await super.startTransaction(isolationLevel);
+    this.isTransactionCommand = false;
+  }
+
+  async commitTransaction(): Promise<void> {
+    this.isTransactionCommand = true;
+    await super.commitTransaction();
+    this.isTransactionCommand = false;
+  }
+
+  async rollbackTransaction(): Promise<void> {
+    this.isTransactionCommand = true;
+    await super.rollbackTransaction();
+    this.isTransactionCommand = false;
   }
 }
